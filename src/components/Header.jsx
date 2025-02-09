738292fd37869ff7b57df6a56875946ba5ab7533
Shourya Batra <51655190+ShouryaBatra@users.noreply.github.com>
2025-02-09 10:37:57 -0800

    update header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,9 +3,9 @@ import React from "react";
 function Header() {
   return (
     <header className="text-center mb-5">
-      <h1 className="text-4xl font-bold text-gray-800">My To-Do List</h1>
+      <h1 className="text-4xl font-bold text-gray-800">Dynamic To-Do List</h1>
       <p className="text-gray-600 italic mt-2">
-        Stay organized and productive!
+        Made with React and TailwindCSS
       </p>
     </header>
   );
